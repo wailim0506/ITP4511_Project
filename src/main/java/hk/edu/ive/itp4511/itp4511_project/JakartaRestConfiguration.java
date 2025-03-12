cde4257bbaf5398bc130b5c67a23a419cf0f67eb
wailim0506 <wailim0506@icloud.com>
2025-03-13 01:25:42 +0800

    Initial commit

---
+++ src/main/java/hk/edu/ive/itp4511/itp4511_project/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package hk.edu.ive.itp4511.itp4511_project;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
